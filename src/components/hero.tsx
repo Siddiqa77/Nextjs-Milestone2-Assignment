c2ce90d7009ec99ba7416fb129f191711b35886c
Siddiqa Badar <147896187+Siddiqa77@users.noreply.github.com>
2024-11-10 00:27:04 +0500

    hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -49,20 +49,20 @@ export default function HeroSection() {
         {/* Social Media Icons on the Left */}
 
         <div className="absolute top-20 left-0 flex flex-col items-center space-y-4 ml-4 text-white md:ml-8">
-          <Image className="h-[190px] w-[1px]" src={Vector12} alt="" width={10} height={10} />
+          <Image className="h-[190px] w-[1px]" src={vector12} alt="" width={10} height={10} />
           <FaFacebookF className="cursor-pointer" />
           <FaInstagram className="cursor-pointer" />
           <FaTwitter className="cursor-pointer" />
-          <Image className="h-[190px] w-[1px]" src={Vector11} alt="" />
+          <Image className="h-[190px] w-[1px]" src={vector11} alt="" />
         </div>
 
         {/* Social Media Icons on the Right */}
         <div className="absolute top-20 right-0 flex flex-col items-center space-y-4 mr-4 text-white md:mr-8">
-          <Image className="h-[190px] w-[1px]" src={Vector12} alt="" />
+          <Image className="h-[190px] w-[1px]" src={vector12} alt="" />
           <HiUserCircle className="cursor-pointer" />
           <HiBell className="cursor-pointer" />
           <HiShoppingCart className="cursor-pointer" />
-          <Image className="h-[190px] w-[1px]" src={Vector11} alt="" />
+          <Image className="h-[190px] w-[1px]" src={vector11} alt="" />
         </div>
       </div>
       {/* Back to top button */}
