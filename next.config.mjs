f50afe98fd932ff87500e0fc60b55dba20ec2792
Siddiqa Badar <siddiqaasim778899@gmail.com>
2025-03-01 00:55:36 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['jaracake.com', 'glamorganbakery.com', 'i.pinimg.com', 'blogger.googleusercontent.com', 'www.eatlanka.com'], // Add the external domain here
+    },
+  };
+  
+  export default nextConfig;
+  
